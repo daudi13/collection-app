ee15f5494191afd718b03260878cffef94826546
daudi13 <davouma13@gmail.com>
2024-02-07 22:37:45 +0300

    Add userReducer, albumReducer and photoReducer for handling their respective state logic

--- src/appstore/store.js
+++ src/appstore/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../feature/users/usersSlice"
+import albumReucer from "../feature/albums/albumsSlice"
+import photoReducer from "../feature/photos/photosSlice"
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    users: userReducer,
+    albums: albumReucer,
+    photo: photoReducer
+  }
 })
