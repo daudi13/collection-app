c0aad0b2fb97d64473269b0a6fbcf414476e0560
daudi13 <davouma13@gmail.com>
2024-02-08 10:51:14 +0300

    Use builder callback since support for createSlice.extraReducer was removed

--- src/feature/photos/photosSlice.js
+++ src/feature/photos/photosSlice.js
@@ -17,18 +17,19 @@ export const fetchPhotos = createAsyncThunk("photos/fetchPhotos", async () => {
 const photoSlice = createSlice({
   name: "photos",
   initialState,
-  extraReducers: {
-    [fetchPhotos.pending]: (state) => {
-      state.isLoading = true
-    },
-    [fetchPhotos.fulfilled]: (state, action) => {
-      state.isLoading = false
-      state.photos = action.payload
-    },
-    [fetchPhotos.rejected]: (state, action) => {
-      state.isLoading = false
-      state.error = action.error.message
-    }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPhotos.pending, (state) => {
+      state.status = "loading"
+      })
+      .addCase(fetchPhotos.fulfilled, (state, action) => {
+        state.status = "success"
+        state.albums = action.payload
+      })
+      .addCase(fetchPhotos.rejected, (state, action) => {
+        state.status = "error"
+        state.error = action.error.message
+    })
   }
 });
 
