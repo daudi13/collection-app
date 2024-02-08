a4a783f08062a9d2c1ec6fedc99d3e38f0f88d80
daudi13 <davouma13@gmail.com>
2024-02-08 10:50:21 +0300

    Use builder callback since support for createSlice.extraReducer was removed

--- src/feature/albums/albumsSlice.js
+++ src/feature/albums/albumsSlice.js
@@ -5,7 +5,7 @@ const URL = "https://jsonplaceholder.typicode.com/albums"
 
 const initialState = {
   albums: [],
-  isLoading: false,
+  status: "idle",
   error: null
 }
 
@@ -17,23 +17,24 @@ export const fetchAlbums = createAsyncThunk("albums/fetchAlbums", async () => {
 const albumSlice = createSlice({
   name: "albums",
   initialState,
-  extraReducers: {
-    [fetchAlbums.pending]: (state) => {
-      state.isLoading = true
-    },
-    [fetchAlbums.fulfilled]: (state, action) => {
-      state.isLoading = false
-      state.albums = action.payload
-    },
-    [fetchAlbums.rejected]: (state, action) => {
-      state.isLoading = false
-      state.error = action.error.message
-    }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAlbums.pending, (state) => {
+      state.status = "loading"
+      })
+      .addCase(fetchAlbums.fulfilled, (state, action) => {
+        state.status = "success"
+        state.albums = action.payload
+      })
+      .addCase(fetchAlbums.rejected, (state, action) => {
+        state.status = "error"
+        state.error = action.error.message
+    })
   }
 });
 
 export const selectAllAlbums = state => state.albums.albums
-export const getAlbumLoadingStatus = state => state.albums.isLoading
+export const getAlbumStatus = state => state.albums.status
 export const getAlbumError = state => state.albums.error
 export const getAlbumsById = (state, albumId) => state.albums.albums.find(album => album.id === albumId)
 export default albumSlice.reducer
