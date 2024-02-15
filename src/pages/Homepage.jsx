3bbfd660a11594742eff324533afa7b68f2c6488
daudi13 <davouma13@gmail.com>
2024-02-15 17:07:31 +0300

    Add Home page that will display all users when the page loads

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { firebaseConfig } from '../firebase';
-import { CollectionState } from '../CollectionContext';
-import { Container, Typography, Button } from '@mui/material';
+import { Container } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
-import Hero from "../assets/hero.png"
+
 import { fetchUsers, getUserStatus, selectAllUsers } from '../feature/users/usersSlice';
 import UserCard from '../components/UserCard';
+import { useNavigate } from 'react-router-dom';
+import { CollectionState } from '../CollectionContext';
+import LandingPage from './LandingPage';
 
 
 const Homepage = () => {
   const users = useSelector(selectAllUsers);
   const status = useSelector(getUserStatus)
   const dispatch = useDispatch();
-  const { setOpen, open, user } = CollectionState();
+  const navigate = useNavigate()
+
+  const { isAuthenticated } = CollectionState();
 
   useEffect(() => {
     if (status === "idle") dispatch(fetchUsers())
@@ -21,13 +24,6 @@ const Homepage = () => {
   
   
     const useStyles = makeStyles()((theme) => ({
-      wrapper: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        height: "100vh",
-        overflowY: "hidden",
-      },
       cards: {
       display: "flex",
       flexWrap: "wrap",
@@ -36,13 +32,6 @@ const Homepage = () => {
       marginTop: 70,
       gap: 40,
       },
-      textBox: {
-        width: 450
-      },
-      content: {
-        marginBottom: 30,
-        textAlign: "justify"
-      },
       boxCard: {
       width: 250,
       padding: 15,
@@ -50,46 +39,28 @@ const Homepage = () => {
       borderColor: "black",
       borderStyle: "solid",
       borderRadius: 20
-    }
+      },
+      loadingBox:{ 
+      display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      }
     }))
   
   const { classes } = useStyles()
 
   const userContent = users?.map((user) => (
-    <div key={user?.id} className={classes.boxCard}>
+    <div key={user?.id} className={classes.boxCard} onClick={() => navigate(`/user/${user.id}`)}>
       <UserCard user={user} />
     </div>
   ));
 
-  
-  if (user) {
-    return (
-      <Container className={classes.cards}>
-      {userContent}
-      </Container>
-    )
-  }
-
   return (
-    <Container className={classes.wrapper}>
-      <div className={classes.textBox}>
-        <Typography className={classes.content}>
-            SnapHub is an immersive online platform that seamlessly intertwines the worlds of photography, storytelling, and global connection. At its core, SnapHub is a community-driven space where individuals from around the world converge to share the stories captured through their lenses.
-            SnapHub goes beyond a typical photo-sharing platform; it's a sanctuary for visual storytellers. Users craft narratives through their albums and photos, transforming moments into captivating tales that resonate with a diverse and engaged audience
-        </Typography>
-        <Button variant="contained" style={{
-          width: 215,
-          height: 40,
-          backgroundColor: "#53c28b",
-          color: "black"
-        }}>
-          create an account
-        </Button>
-      </div>
-      <img src={Hero} style={{
-        width: 560
-      }} alt="hero"/>
-    </Container>
+    isAuthenticated ? (
+      <Container className = {classes.cards}>
+      {userContent}
+      </Container>) : <LandingPage/>
   )
 }
 
