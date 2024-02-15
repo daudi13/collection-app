97e907c37231a3437d4da06df9866ccd70c0f4ee
daudi13 <davouma13@gmail.com>
2024-02-15 17:54:01 +0300

    Add Usercard component for displaying user info for each user card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,14 +1,29 @@
 import { makeStyles } from 'tss-react/mui';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaUser } from "react-icons/fa";
-import { Container } from '@mui/material';
+import axios from 'axios';
 
 const UserCard = ({ user }) => {
+  const [albums, setAlbums] = useState();
+
+  const URL = `https://jsonplaceholder.typicode.com/users/${user.id}/albums`;
+
+  const fetchUserAlbums = async () => {
+    const { data } = await axios.get(URL)
+    setAlbums(data)
+  }
+
+  useEffect(() => {
+    fetchUserAlbums()
+  }, [])
+  
+  console.log(albums?.length)
+
   return (
     <div>
       <FaUser size={200}/>
       <p><span>Name:</span> {user?.name}</p>
-      <p><span>Album No:</span>10</p>
+      <p><span>Album No:</span>{albums?.length}</p>
     </div>
   )
 }
