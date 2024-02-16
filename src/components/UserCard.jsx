4eadd7583579ce992205e9851384bc8e209c3f33
daudi13 <davouma13@gmail.com>
2024-02-16 11:42:08 +0300

    Add loading conditional to display loader when the loading status is true

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,29 +1,32 @@
-import { makeStyles } from 'tss-react/mui';
 import React, { useEffect, useState } from 'react'
 import { FaUser } from "react-icons/fa";
 import axios from 'axios';
+import { CircularProgress } from '@mui/material';
+import { CollectionState } from '../CollectionContext';
+
 
 const UserCard = ({ user }) => {
   const [albums, setAlbums] = useState();
+  const { loading, setLoading } = CollectionState();
 
   const URL = `https://jsonplaceholder.typicode.com/users/${user.id}/albums`;
 
   const fetchUserAlbums = async () => {
+    setLoading(true)
     const { data } = await axios.get(URL)
     setAlbums(data)
+    setLoading(false)
   }
 
   useEffect(() => {
     fetchUserAlbums()
   }, [])
-  
-  console.log(albums?.length)
 
   return (
     <div>
       <FaUser size={200}/>
       <p><span>Name:</span> {user?.name}</p>
-      <p><span>Album No:</span>{albums?.length}</p>
+      <p><span>Album No:</span>{loading ? <CircularProgress /> : albums?.length}</p>
     </div>
   )
 }
